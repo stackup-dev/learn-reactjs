0a9f8dae4ca2f1fb21e0ab8f8f2ed0a50ec4e93b
StackUp <110076027+stackup-dev@users.noreply.github.com>
2023-02-08 20:02:14 +0800

    update

---
+++ foodary/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () =>{
+    return(
+        <div style={{display:"flex",flexDirection:"row",alignItems:"center",justifyContent:"center"}}>
+            <p>Stackie's Foodary © 2023</p>
+    </div>
+    )
+}
+    
+export default Footer;
---
+++ foodary/recipe.js
@@ -0,0 +1,37 @@
+export const my_recipe = {
+    recipe_1: {
+        name: 'Chirashi Don',
+        preparation_time: '30 minutes',
+        servings: '2',
+        ingredients: [
+            '1/2 cup rice',
+            '1/2 cup water',
+            '2 eggs',
+            '300g of salmon, tuna and salmon roe',
+        ],
+        instructions: [
+            'Cook rice and water in a rice cooker',
+            'Fry eggs in a pan',
+            'Serve rice, eggs and fish on a plate',
+        ],
+        image: '/img/recipe1.jpg'
+ 
+    },
+    recipe_2: {
+        name: 'Classic Quiche',
+        preparation_time: '25 minutes',
+        servings: '3',
+        ingredients: [
+            'pie crust',
+            '1 cup of heavy cream',
+            '4 eggs',
+            'salt and pepper to taste'
+        ],
+        instructions: [
+            'Blind bake the pie crust',
+            'Combine eggs and heavy cream in a bowl',
+            'Pour the filling into the pie crust and bake for 45 minutes',
+        ],
+        image:'/img/recipe2.jpg'
+    }
+}
