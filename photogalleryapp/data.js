ec93cd32843511a947d65554738fc12c027dfff0
StackUp <110076027+stackup-dev@users.noreply.github.com>
2023-06-12 23:28:03 +0800

    Add files via upload

---
+++ photogalleryapp/data.js
@@ -0,0 +1,65 @@
+export const data = [
+    {
+      "name": "Photo 1",
+      "url": "/images/sg1.jpeg",
+      "description": "Photo 1 description",
+      "date": "2020-01-01",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 2",
+      "url": "/images/sg2.jpeg",
+      "description": "Photo 2 description",
+      "date": "2020-01-02",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 3",
+      "url": "/images/sg3.jpeg",
+      "description": "Photo 3 description",
+      "date": "2020-01-03",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 4",
+      "url": "/images/insta_4.jpg",
+      "description": "Photo 4 description",
+      "date": "2020-01-04",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 5",
+      "url": "/images/insta_5.jpg",
+      "description": "Photo 5 description",
+      "date": "2020-01-05",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 6",
+      "url": "/images/insta_6.jpg",
+      "description": "Photo 6 description",
+      "date": "2020-01-06",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 7",
+      "url": "/images/insta_7.jpg",
+      "description": "Photo 7 description",
+      "date": "2020-01-07",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 8",
+      "url": "/images/insta_8.jpg",
+      "description": "Photo 8 description",
+      "date": "2020-01-08",
+      "type": "image/jpg",
+    },
+    {
+      "name": "Photo 9",
+      "url": "/images/insta_9.jpg",
+      "description": "Photo 9 description",
+      "date": "2020-01-09",
+      "type": "image/jpg",
+    }
+  ]
